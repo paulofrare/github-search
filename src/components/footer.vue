ce72eea263d22bf3dc194d9d6d0133348458c64f
Paulo Frare <pfrare.tads@gmail.com>
2020-09-27 03:37:06 -0300

    Refactor: Ajustes

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="footer">© 2020 Paulo Frare | Todos os diretos reservados</div>
+  <div class="footer">
+    <p class="text-footer">© 2020 Paulo Frare | Todos os diretos reservados</p>
+  </div>
 </template>
 
 <script>
@@ -9,13 +11,19 @@ export default {};
 <style>
 .footer {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
+  justify-content: center;
   width: 100%;
   height: 50px;
   color: rgb(233, 233, 233);
   background-color: rgb(39, 39, 39);
   position: fixed;
   bottom: 0;
+}
+
+.text-footer {
+  margin-top: 15px;
+  text-align: center;
 }
 </style>
